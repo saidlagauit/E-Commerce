9a6521b5b04b0c21702623d75b43ad8a47fa0516
Said Lagauit <lagaouit.said@gmail.com>
2023-06-29 16:36:49 +0000

    Remove lazy sizes

--- templates/footer.php
+++ templates/footer.php
@@ -5,7 +5,6 @@
 </footer>
 <script src="<?php echo $js ?>jquery-3.7.0.min.js"></script>
 <script src="<?php echo $js ?>bootstrap.bundle.min.js"></script>
-<script src="<?php echo $js ?>lazysizes.min.js"></script>
 <script src="<?php echo $js ?>main.js"></script>
 
 </body>
