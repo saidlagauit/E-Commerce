d3b217195d0e4a1c5ff414ae77610277dbe48c35
Said Lagauit <lagaouit.said@gmail.com>
2023-06-30 13:46:33 +0000

    add page lang ar

---
+++ inc/languages/ar.php
@@ -0,0 +1,16 @@
+<?php
+$lang = array(
+  'Said Lagauit' => 'سعيد لكويط',
+  'Ltr' => 'rtl',
+  'En' => 'ar',
+  'Bootstrap' => 'bootstrap.rtl.min.css',
+  'English' => 'انجليزي',
+  'Arabic' => 'عربي',
+  'English' => 'انجليزي',
+  'Arabic' => 'عربي',
+  'Shop' => 'متجر',
+  'FAQ' => 'الأسئلة الشائعة',
+  'Tracking' => 'تتبع',
+  'Contact Us' => 'اتصل بنا',
+  '' => ''
+);
