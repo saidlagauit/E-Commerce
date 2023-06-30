07fc5ad3ba85d035988e853179de4aa476070a73
Said Lagauit <lagaouit.said@gmail.com>
2023-06-30 13:41:42 +0000

    active lang en

--- inc/languages/en.php
+++ inc/languages/en.php
@@ -1,3 +1,14 @@
 <?php
-$lang = array ();
-?>
+$lang = array(
+  'Said Lagauit' => 'Said Lagauit',
+  'Ltr' => 'ltr',
+  'En' => 'en',
+  'Bootstrap' => 'bootstrap.min.css',
+  'English' => 'English',
+  'Arabic' => 'Arabic',
+  'Shop' => 'Shop',
+  'FAQ' => 'FAQ',
+  'Tracking' => 'Tracking',
+  'Contact Us' => 'Contact Us',
+  '' => ''
+);
--- templates/header.php
+++ templates/header.php
@@ -1,14 +1,14 @@
 <!doctype html>
-<html lang="en" data-bs-theme="light">
+<html dir="<?php echo $lang['Ltr']; ?>" lang="<?php echo $lang['En']; ?>" data-bs-theme="light">
 
 <head>
   <meta charset="UTF-8" />
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Said Lagauit | <?php getTitle() ?></title>
+  <title><?php echo $lang['Said Lagauit'] . ' | ' . $lang[getTitle()] ?></title>
   <meta name="description" content="Discover a wide range of high-quality digital products at the Said Lagauit Online Store. Shop for software, applications, games, and more. Enjoy great deals, fast delivery, and exceptional customer service. Start your online shopping experience for digital products today!" />
   <link rel="shortcut icon" href="<?php echo $img ?>favicon_io/favicon.ico" type="image/x-icon">
-  <link rel="stylesheet" href="<?php echo $css ?>bootstrap.min.css" />
+  <link rel="stylesheet" href="<?php echo $css; ?><?php echo $lang['Bootstrap']; ?>" />
   <link rel="stylesheet" href="<?php echo $css ?>all.min.css" />
   <link rel="stylesheet" href="<?php echo $css ?>main.css" />
 
