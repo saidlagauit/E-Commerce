9c14f75ac9baaded1a03a1a04bcadb619e521744
Said Lagauit <lagaouit.said@gmail.com>
2023-06-30 15:41:48 +0000

    fix prb lang

--- templates/header.php
+++ templates/header.php
@@ -26,7 +26,7 @@ if ($selectedLanguage === 'ar') {
   <meta charset="UTF-8" />
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title><?php echo $lang[getTitle()] . ' | ' . $lang['Said Lagauit']; ?></title>
+  <title><?php getTitle(); ?></title>
   <meta name="description" content="Discover a wide range of high-quality digital products at the Said Lagauit Online Store. Shop for software, applications, games, and more. Enjoy great deals, fast delivery, and exceptional customer service. Start your online shopping experience for digital products today!" />
   <link rel="shortcut icon" href="<?php echo $img ?>favicon_io/favicon.ico" type="image/x-icon">
   <link rel="stylesheet" href="<?php echo $css; ?><?php echo $lang['Bootstrap']; ?>" />
